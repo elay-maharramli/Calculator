8898e7cfa3aa94a0d4bb21e03c2064cc96742a88
ElaySuperCoder <36122259+Maharramli@users.noreply.github.com>
2020-02-06 19:27:40 +0400

    calculate buttons added

--- Calculator.py
+++ Calculator.py
@@ -13,6 +13,10 @@ while True:
     numtxt2 = Label(text="İkinci ədəd:",bg="grey",font=("Arial", 15))
     nument = Entry()
     nument2 = Entry()
+    add = Button(text="+",bg="green",width=5)
+    sub = Button(text="-",bg="green",width=5)
+    mul = Button(text="x",bg="green",width=5)
+    div = Button(text="/",bg="green",width=5)
 
 
 
@@ -25,10 +29,12 @@ while True:
 
 
 
-
-
+    add.place(x = 110,y = 200)
+    sub.place(x = 180,y = 200)
+    mul.place(x = 250,y = 200)
+    div.place(x = 320,y = 200)
     nument.place(x = 245,y = 115)
-    nument2.place(x = 235,y = 155)
+    nument2.place(x = 245,y = 155)
     numtxt.place(x = 100,y=110)
     numtxt2.place(x = 100,y=150)
     caltxt.place(x = 190,y = 40)
